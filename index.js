ac6c41de95cb50108ddaf6d89bd7ee632de60561
jaein-tech <55172701+jaein-tech@users.noreply.github.com>
2019-10-03 15:35:50 +0900

    if, else function practice

--- index.js
+++ index.js
@@ -1,9 +1,32 @@
-const age = prompt("How old are you?")
+const title = document.querySelector("#title");
 
-if (age >= 18 && age <= 21) {
-    console.log("you can drink but you should not");
-} else if (age > 21){
-    console.log("go ahed");
-} else {
-    console.log("you cant");
+const BASE_COLOR = "rgb(52, 73, 94)";
+const OTHER_COLOR = "#7f8c8d";
+
+function handleClick() {
+    const currentColor = title.style.color;
+    if (currentColor === BASE_COLOR){
+        title.style.color = OTHER_COLOR;
+    } else {
+        title.style.color = BASE_COLOR;
+    }
 }
+
+function init(){
+    title.style.color = BASE_COLOR;
+    title.addEventListener("mouseenter", handleClick);
+}
+init();
+
+
+//HTMl JavaScript DOM event MDN
+function handleOffline() {
+    console.log("Good bye!")
+}
+
+function handleOnline() {
+    console.log("Welcome back!")
+}
+
+window.addEventListener("offline", handleOffline);
+window.addEventListener("online", handleOnline);
